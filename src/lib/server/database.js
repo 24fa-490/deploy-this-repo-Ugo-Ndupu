522bd5a932d28f8e589a2f45482b485925c45ee6
Ugondupu <ugondupu99@csu.fullerton.edu>
2024-10-27 20:16:48 -0700

    trigger redploy

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -7,3 +7,12 @@ dotenv.config();
 const sql = postgres(process.env.PGCONNECT);
 
 export default sql;
+
+import os
+import psycopg2
+
+# Access the Supabase connection string from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Connect to the database
+conn = psycopg2.connect(DATABASE_URL)
